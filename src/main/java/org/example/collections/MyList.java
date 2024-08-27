8a3e1f30ea1f3a894eecaf25e5f8e43dc6786970
AlexLeo <dudealexlion@gmail.com>
2024-08-26 21:28:17 -0700

    second test

---
+++ src/main/java/org/example/collections/MyList.java
@@ -0,0 +1,4 @@
+package org.example.collections;
+
+public class MyList {
+}
---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FirstTest extends BaseTest{
+
+    @Test
+    public void testFirst() throws InterruptedException {
+        String text = driver.findElement(By.tagName("h1")).getText();
+
+        Assert.assertEquals(text, "AskOmDch");
+    }
+
+    @Test
+    public void testSecond() throws InterruptedException {
+        driver.findElement(By.xpath("//a[@href='/store']")).click();
+        String text = driver.findElement(By.tagName("h1")).getText();
+
+        Assert.assertEquals(text, "Store");
+    }
+}
