45d22b7dd933cd5ed5d5cf6a50dcff29abf89749
Sky8Ann <anna8skl@gmail.com>
2024-08-27 12:50:54 -0700

    Shopping Cart test

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,4 +1,6 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -18,4 +20,31 @@ public class FirstTest extends BaseTest{
 
         Assert.assertEquals(text, "Store");
     }
+
+    @Test
+    public void testShoppingCartIconValidationTest() {
+        driver.findElement(By.xpath("//a[@class='wp-block-button__link']")).click();
+        driver.findElement(By.xpath("//a[contains(@href, '?add-to-cart=1205')]")).click();
+        driver.findElement(By.xpath("//div[@class='site-primary-header-wrap ast-builder-grid-row-container site-header-focus-item ast-container']//span[@class='count'][normalize-space()='1']")).click();
+        String text = driver.findElement(By.xpath("//a[normalize-space()='Basic Blue Jeans']")).getText();
+
+        Assert.assertEquals(text, "Basic Blue Jeans");
+
+    }
+
+    @Test
+    public void ShoppingCartPopUpValidationTest() {
+        driver.findElement(By.xpath("//a[@class='wp-block-button__link']")).click();
+        driver.findElement(By.xpath("//a[@aria-label='Add “Anchor Bracelet” to your cart']")).click();
+        WebElement shoppingCart = driver.findElement(By.xpath("//div[@class='site-primary-header-wrap ast-builder-grid-row-container site-header-focus-item ast-container']//span[@class='count'][normalize-space()='1']"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(shoppingCart).perform();
+        WebElement viewCart = driver.findElement(By.xpath("//div[@class='site-primary-header-wrap ast-builder-grid-row-container site-header-focus-item ast-container']//a[@class='button wc-forward'][normalize-space()='View cart']"));
+        viewCart.click();
+        String text = driver.findElement(By.xpath("//a[normalize-space()='Anchor Bracelet']")).getText();
+
+        Assert.assertEquals(text, "Anchor Bracelet");
+
+    }
 }
+
