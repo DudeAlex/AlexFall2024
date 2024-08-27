373df1ecdc59cc12235aefc3d73db84822c51593
IrynaHryhoriv <114322325+IrynaHryhoriv@users.noreply.github.com>
2024-08-27 14:41:50 -0700

    Iryna h testClickOnStoreButtonAdded (#7)

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -18,4 +18,16 @@ public class FirstTest extends BaseTest{
 
         Assert.assertEquals(text, "Store");
     }
+
+    @Test
+    public void testClickOnStoreButton() throws InterruptedException {
+        String url = "https://askomdch.com/store";
+
+        driver.findElement(By.xpath("//a[@href='/store']")).click();
+
+        String actualResult = driver.getCurrentUrl();
+
+        Assert.assertEquals(actualResult, url);
+
+    }
 }
