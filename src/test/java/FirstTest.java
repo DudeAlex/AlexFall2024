678dccf3c086e8abe31d2cff590aafe1861e3f60
Anna <am-rabota@mail.ru>
2024-09-03 20:20:05 -0700

    Add test on checking Add and Remove single item from the cart

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -219,5 +219,22 @@ public class FirstTest extends BaseTest{
 
         Assert.assertEquals(itemName, itemInTheCart);
     }
+
+    @Test
+    public void testAddAndRemoveSingleItemFromCart() {
+        driver.findElement(By.xpath("//div[@id='ast-desktop-header']//a[text()='Store']")).click();
+
+        driver.findElement(By.xpath("//div[@class='astra-shop-summary-wrap']//a[text()='Add to cart']")).click();
+        WebElement viewCart = driver.findElement(By.linkText("View cart"));
+        String viewCartText = viewCart.getText();
+
+        Assert.assertEquals(viewCartText, "View cart");
+
+        viewCart.click();
+        driver.findElement(By.xpath("//a[@class = 'remove']")).click();
+
+        String itemRemovedMassage = driver.findElement(By.xpath("//*[contains(text(),'removed')]")).getText();
+        Assert.assertTrue(itemRemovedMassage.contains("removed"));
+    }
 }
 
