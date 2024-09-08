b641acc74e41ecd71205305e2659a431125de1e0
Alex Motornyi <oleksandr.motornyi@gmail.com>
2024-09-08 15:01:32 -0700

    New Branch

---
+++ src/test/java/Draft.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class Draft {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.get("https://askomdch.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+
+    }
+}
+
---
+++ src/test/java/ShopButtonTest.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ShopButtonTest extends Draft {
+    @Test
+    public void Button() throws InterruptedException {
+        driver.findElement(By.xpath("//a[@class='wp-block-button__link']")).click();
+        Thread.sleep(4000);
+        String correctUrl = driver.getCurrentUrl();
+        Assert.assertEquals(correctUrl,"https://askomdch.com/store");
+    }
+
+}
+
