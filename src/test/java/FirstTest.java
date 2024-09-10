1463976fe0fba6bf46441e9ac474c671ca04ddd6
Turii <o.turij@gmail.com>
2024-09-09 18:20:36 -0700

    Check that items have lower price

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -398,5 +398,27 @@ public class FirstTest extends BaseTest {
         
         Assert.assertEquals(errorText, invalidEmailErrorMsg);
     }
+
+    @Test
+    public void check20offPriceOnHoePageOnHoePage() {
+        List<WebElement> products = driver.findElements(By.cssSelector("ul.products li.product"));
+
+        for (WebElement product : products) {
+            if (product.findElements(By.className("onsale")).size() > 0) {
+
+                WebElement oldPriceElement = product.findElement(By.cssSelector("del .woocommerce-Price-amount"));
+                WebElement newPriceElement = product.findElement(By.cssSelector("ins .woocommerce-Price-amount"));
+
+                double oldPrice = Double.parseDouble(oldPriceElement.getText().replace("$", ""));
+                double newPrice = Double.parseDouble(newPriceElement.getText().replace("$", ""));
+
+                Assert.assertTrue(newPrice < oldPrice, "New price less then old");
+                System.out.println("Off 20%: " + product.getText());
+            } else {
+                System.out.println("Without Off: " + product.getText());
+            }
+        }
+    }
+
 }
 
