ac345c54c7f058c29e54fa1e39ca2fc23a1ed6e1
Anna <am-rabota@mail.ru>
2024-09-11 09:26:26 -0700

    Adding test for quantity update in cart

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -243,6 +243,7 @@ public class FirstTest extends BaseTest {
         Assert.assertEquals(itemName, itemInTheCart);
     }
 
+    /* ANNA TEST - START */
     @Test
     public void testAddAndRemoveSingleItemFromCart() {
         driver.findElement(By.xpath("//div[@id='ast-desktop-header']//a[text()='Store']")).click();
@@ -259,6 +260,55 @@ public class FirstTest extends BaseTest {
         String itemRemovedMassage = driver.findElement(By.xpath("//*[contains(text(),'removed')]")).getText();
         Assert.assertTrue(itemRemovedMassage.contains("removed"));
     }
+
+    @Test
+    public void testUpdateQuantityInCart() {
+        driver.findElement(By.xpath("//div[@id='ast-desktop-header']//a[text()='Store']")).click();
+
+        driver.findElement(By.xpath("//div[@class='astra-shop-summary-wrap']//a[text()='Add to cart']")).click();
+        WebElement viewCart = driver.findElement(By.linkText("View cart"));
+        String viewCartText = viewCart.getText();
+
+        Assert.assertEquals(viewCartText, "View cart");
+
+        viewCart.click();
+
+        String priceStr = driver.findElement(By.xpath("//td[@class = 'product-subtotal']/span[@class='woocommerce-Price-amount amount']")).getText();
+        if(priceStr.charAt(0) == '$'){ //since the price is a string that has $, I need to remove the $ first
+            priceStr = priceStr.substring(1); //creating the substring without $
+        }
+        double price = Double.valueOf(priceStr); //change string price to double
+
+        WebElement quantity = driver.findElement(By.xpath("//input[@class='input-text qty text']"));
+        quantity.click();
+        quantity.clear();
+        quantity.sendKeys("2");
+
+        String updatedPriceString = driver.findElement(By.xpath("//td[@class = 'product-subtotal']/span[@class='woocommerce-Price-amount amount']")).getText();
+        driver.findElement(By.xpath("//button[@name='update_cart']")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        String finalUpdatedPriceStr = updatedPriceString;
+        // After the clicking Update price button, I need wait until the price will be updated
+        wait.until(driver -> {
+            String checkPrice = driver.findElement(By.xpath("//td[@class = 'product-subtotal']/span[@class='woocommerce-Price-amount amount']")).getText();
+            if(checkPrice.equals(finalUpdatedPriceStr)) { // checking if price still equal to original price (before update)
+                return false;
+            }
+            return true;  //when price is updated (not equal anymore) returning true
+        });
+
+        //  Getting the updated subtotal price
+        String updatedPriceStr = driver.findElement(By.xpath("//td[@class = 'product-subtotal']/span[@class='woocommerce-Price-amount amount']")).getText();
+        if(updatedPriceStr.charAt(0) == '$'){
+            updatedPriceStr = updatedPriceStr.substring(1);
+        }
+
+        double updatedPrice = Double.valueOf(updatedPriceStr);
+        Assert.assertEquals(updatedPrice, (price * 2));
+    }
+
+    /* ANNA TEST - END */
 
     @Test
     public void testSearchForJeans() {
