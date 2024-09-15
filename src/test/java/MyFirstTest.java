9775abbf15646d750a31ff5bc02eaa913153199c
MIKHAIL IAKOVENKO <114107429+yakovenko79@users.noreply.github.com>
2024-09-15 11:22:41 +0300

    fix: second test, add comments

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import java.time.Duration;
-import java.util.ArrayList;
 import java.util.List;
 
 public class MyFirstTest extends BaseTest {
@@ -31,9 +30,11 @@ public class MyFirstTest extends BaseTest {
 
     @Test
     public void isClickOnEveryHeaderElementEqualsToCorrEndpoint() {
+        //---------------------------------------------------------------------------------------------------------
         //TODO:
         // 1. Click on every element in the top navbar,
         // 2. Compare actual url and expected url
+        //---------------------------------------------------------------------------------------------------------
 
         List<WebElement> menuItems = driver.findElement(By.id("ast-hf-menu-1")).findElements(By.tagName("li"));
         for (int i = 0; i < PAGES.size(); i++) {
@@ -48,31 +49,56 @@ public class MyFirstTest extends BaseTest {
         }
     }
 
+    //==============================================================================================================
+
     @Test
     public void isSaleElementContainsSaleLabel() {
+        //------------------------------------------------------------------------------
         //TODO:
         // 1. Go to Store,
-        // 2. choose first product element,
-        // 3. check the icon "Sale" overlaps product picture
-        // 4. Go to Store page and check that elements for sale contain "Sale" label
+        // 2. choose list of products,
+        // 3. get every element and check if the product has a tag named "del"
+        // 4. if the product has a tag named "del" check then it must have a "Sale" tag
+        // 5. use check that "Sale" label overlaps product element
+        //------------------------------------------------------------------------------
 
+        //Go to store
         driver.get("https://askomdch.com/store/");
+
+        //waiting for the products to appear on the screen
         waitForElementPresent(
                 By.cssSelector("div.ast-woocommerce-container>ul.products"),
                 "Products are not visible",
                 Duration.ofSeconds(5));
-//        WebElement firstProduct = driver.findElement(By.cssSelector("ul.products li.product:nth-child(1)"));
-        List<WebElement> products = driver.findElements(By.cssSelector("ul.products"));
+
+        //Get list of products
+        List<WebElement> products = driver.findElements(By.cssSelector("div ul.products li"));
+        System.out.println("Amount of products in list: " + products.size());
+
+        //Display names of all products
+        for(WebElement product : products){
+            System.out.println("Product on the page: " + product.findElement(By.cssSelector("ul li h2")).getText());
+        }
+        //Get every product and check
         for (int i = 0; i < products.size(); i++) {
+
+            //Check if the product has a tag named "del" then it should have a "Sale" label/
             if (products.get(i).findElements(By.tagName("del")).size() > 0) {
                 WebElement firstProductPict = driver.findElement(By.cssSelector("ul.products li.product:nth-child(1) a:nth-child(2)"));
                 WebElement saleLabel = driver.findElement(By.cssSelector("ul.products li.product:nth-child(1) span.onsale"));
                 Rectangle rect1 = firstProductPict.getRect();
                 Rectangle rect2 = saleLabel.getRect();
+
+                //check coordinates
                 boolean isOverlapping = rect1.getX() < (rect2.getX() + rect2.getWidth()) &&
                         (rect1.getX() + rect1.getWidth()) > rect2.getX() &&
                         rect1.getY() < (rect2.getY() + rect2.getHeight()) &&
                         (rect1.getY() + rect1.getHeight()) > rect2.getY();
+
+                //Output products with "Sale" label
+                System.out.println("Product for sale: " + products.get(i).findElement(By.cssSelector("ul li h2")).getText());
+
+                //Assert
                 Assert.assertTrue(isOverlapping, "Sale icon doesn't overlap product picture");
             }
 
