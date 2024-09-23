c4ceba9188bd7bccd0ee2d962701b60242a466af
Turii <o.turij@gmail.com>
2024-09-23 15:54:21 -0700

    Add Cart is empty Tests

--- src/test/java/com/ecommerce/tests/components/header/NavigationMenuTest.java
+++ src/test/java/com/ecommerce/tests/components/header/NavigationMenuTest.java
@@ -1,6 +1,9 @@
+package com.ecommerce.tests.components.header;
+
 import com.ecommerce.base.BaseTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -32,36 +35,19 @@ public class NavigationMenuTest extends BaseTest {
         return activeMenuItem.getCssValue("color");
     }
 
-    @Test
+    @Test(description = "10.1 | RF >  Verify active page is highlighted in the navigation bar # https://app.clickup.com/t/8689ufb6d")
     public void testHomeMenuHighlighted() {
         // get actual color
         String actualColor = getActiveMenuItemColor("menu-item-1226");
-
         // Expected color
         String expectedColor = "rgba(255, 255, 255, 1)";
-        // Find the "Home" menu item by its ID
-        //WebElement accessoriesMenuItem = getMenuItemById("menu-item-1226"); // ID для Home
-        // Click on the "Home" menu item
-        //accessoriesMenuItem.click();
 
-        // Use the 'wait' object from com.ecommerce.base.BaseTest to wait until the "Home" menu item becomes active
-        // This wait will ensure that the element with the class 'current-menu-item' is present in the DOM
-        //WebElement activeMenuItem = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("li.current-menu-item a")));
-
-        // Get the CSS property 'color' of the active menu item (the "Home" menu item)
-        //WebElement activeMenuItem = driver.findElement(By.cssSelector("li.current-menu-item a"));
-
-        // Get the CSS property 'color' of the active menu item (the "Home" menu item)
-        //String color = activeMenuItem.getCssValue("color");
-        //System.out.println("The color of the active menu item is: " + color);
-
-        // Verify that the color has changed to the expected purple color (rgba(96, 43, 183, 1))
         Assert.assertEquals(actualColor, expectedColor, "The 'Home' menu item should be highlighted in the expected color.");
 
     }
 
 
-    @Test
+    @Test(description = "10.1 | RF >  Verify active page is highlighted in the navigation bar # https://app.clickup.com/t/8689ufb6d")
     public void testStoreMenuHighlighted() {
         String actualColor = getActiveMenuItemColor("menu-item-1227");
         String expectedColor = "rgba(96, 43, 183, 1)";
@@ -71,7 +57,7 @@ public class NavigationMenuTest extends BaseTest {
 
 
 
-    @Test
+    @Test(description = "10.1 | RF >  Verify active page is highlighted in the navigation bar # https://app.clickup.com/t/8689ufb6d")
     public void testMenMenuHighlighted() {
         String actualColor = getActiveMenuItemColor("menu-item-1228");
         String expectedColor = "rgba(96, 43, 183, 1)";
@@ -80,7 +66,7 @@ public class NavigationMenuTest extends BaseTest {
     }
 
 
-    @Test
+    @Test(description = "10.1 | RF >  Verify active page is highlighted in the navigation bar # https://app.clickup.com/t/8689ufb6d")
     public void testWomenMenuHighlighted() {
         String actualColor = getActiveMenuItemColor("menu-item-1229");
         String expectedColor = "rgba(96, 43, 183, 1)";
@@ -88,7 +74,7 @@ public class NavigationMenuTest extends BaseTest {
         Assert.assertEquals(actualColor, expectedColor, "The 'Home' menu item should be highlighted in the expected color.");
     }
 
-    @Test
+    @Test(description = "10.1 | RF >  Verify active page is highlighted in the navigation bar # https://app.clickup.com/t/8689ufb6d")
     public void testAccessoriesMenuHighlighted() {
         String actualColor = getActiveMenuItemColor("menu-item-1230");
         String expectedColor = "rgba(96, 43, 183, 1)";
@@ -96,7 +82,7 @@ public class NavigationMenuTest extends BaseTest {
         Assert.assertEquals(actualColor, expectedColor, "The 'Home' menu item should be highlighted in the expected color.");
     }
 
-    @Test
+    @Test(description = "10.1 | RF >  Verify active page is highlighted in the navigation bar # https://app.clickup.com/t/8689ufb6d")
     public void testAccountMenuHighlighted() {
         String actualColor = getActiveMenuItemColor("menu-item-1237");
         String expectedColor = "rgba(255, 255, 255, 1)";
@@ -104,7 +90,7 @@ public class NavigationMenuTest extends BaseTest {
         Assert.assertEquals(actualColor, expectedColor, "The 'Home' menu item should be highlighted in the expected color.");
     }
 
-    @Test
+    @Test(description = "10.1 | RF >  Verify active page is highlighted in the navigation bar # https://app.clickup.com/t/8689ufb6d")
     public void testAbout(){
         String actualColor = getActiveMenuItemColor("menu-item-1232");
         String expectedColor = "rgba(255, 255, 255, 1)";
@@ -112,12 +98,47 @@ public class NavigationMenuTest extends BaseTest {
         Assert.assertEquals(actualColor, expectedColor, "The 'Home' menu item should be highlighted in the expected color.");
     }
 
-    @Test
+    @Test(description = "10.1 | RF >  Verify active page is highlighted in the navigation bar # https://app.clickup.com/t/8689ufb6d")
     public void testContactUs(){
         String actualColor = getActiveMenuItemColor("menu-item-1233");
         String expectedColor = "rgba(255, 255, 255, 1)";
 
         Assert.assertEquals(actualColor, expectedColor, "The 'Home' menu item should be highlighted in the expected color.");
+    }
+
+    @Test(description = "10.1 - 8.2 |  Verify Cart is empty when clicking the cart icon # https://app.clickup.com/t/8689wehf2")
+    public void testCartIsEmpty() {
+        // Locate the shopping cart icon/button and click it
+        WebElement cartIcon = driver.findElement(By.cssSelector("a[href='https://askomdch.com/cart/']"));
+        cartIcon.click();
+
+        // Wait for the cart to display the message and capture it
+        WebElement emptyCartMessage = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//p[@class = 'cart-empty woocommerce-info']")));
+
+        // Verify the message is "No products in the cart."
+        String expectedMessage = "Your cart is currently empty.";
+        String actualMessage = emptyCartMessage.getText();
+
+        Assert.assertEquals(actualMessage, expectedMessage, "The cart should be empty when no products are added.");
+    }
+
+    @Test(description = "10.1 - 8.2 |  Verify Cart is empty when clicking the cart icon # https://app.clickup.com/t/8689wej0n")
+    public void testCartHoverDisplaysEmptyMessage() {
+        // Locate the cart icon
+        WebElement cartIcon = driver.findElement(By.cssSelector("a[href='https://askomdch.com/cart/']"));
+
+        // Hover over the cart icon
+        Actions action = new Actions(driver);
+        action.moveToElement(cartIcon).perform();
+
+        // Wait for the message to appear
+        WebElement hoverMessage = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("p.woocommerce-mini-cart__empty-message")));
+
+        // Verify the message is correct
+        String expectedMessage = "No products in the cart.";
+        String actualMessage = hoverMessage.getText();
+
+        Assert.assertEquals(actualMessage, expectedMessage, "The cart should display 'No products in the cart' on hover.");
     }
 
 }
