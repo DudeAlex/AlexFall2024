5d0eb3b2a9ffee823b1948bacf4f279a1e2cb51c
Dmitriy Grishin <grishindu@gmail.com>
2024-09-22 22:54:04 -0500

    Create AboutUsNavigationTest.java
    Added two tests for navigation to About page. Current set up is missing POM, work in progress

---
+++ src/test/java/com/ecommerce/tests/about/AboutUsNavigationTest.java
@@ -0,0 +1,26 @@
+package com.ecommerce.tests.about;
+
+import com.ecommerce.base.BaseTest;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AboutUsNavigationTest extends BaseTest {
+
+    @Test (description = "*-* | TC > Navigation to About Us from the Top bar")
+    public void testAboutUsFromTopBar () {
+        driver.findElement(By.xpath("(//*[contains(text(),'About')])[1]")).click(); //xPath = //*[@id="menu-item-1232"]/a
+
+        String aboutUsHeader = driver.findElement(By.className("has-text-align-center")).getText();
+        Assert.assertEquals(aboutUsHeader, "About Us");
+    }
+
+    @Test (description = "*-* | TC > Navigation to About Us from Footer")
+    public void testAboutUsFromFooter() {
+        driver.findElement(By.xpath("(//*[contains(text(),'About')])[3]")).click(); //xPath = //*[@id="menu-item-1255"]/a
+
+        String aboutUsHeader = driver.findElement(By.className("has-text-align-center")).getText();
+        Assert.assertEquals(aboutUsHeader, "About Us");
+    }
+
+}
