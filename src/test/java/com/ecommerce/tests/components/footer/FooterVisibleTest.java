d43cec99004664fff569fa3434ca653bb9d52bbe
ovdro <ovdro.ovdro@gmail.com>
2024-09-24 21:18:21 +0300

    test checks the visibility of the footer and its location at the bottom of the page

---
+++ src/test/java/com/ecommerce/tests/components/footer/FooterVisibleTest.java
@@ -0,0 +1,28 @@
+package com.ecommerce.tests.components.footer;
+
+import com.ecommerce.base.BaseTest;
+import dev.failsafe.internal.util.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+
+public class FooterVisibleTest extends BaseTest {
+    @Test(description = "11.1-6.1 | TC > Verify Navigation Footer Menu List Quick Links # https://app.clickup.com/t/8689wk9pk ")
+    public void testFooterVisible() {
+
+        driver.get("https://askomdch.com/");
+
+        WebElement footer = driver.findElement(By.tagName("footer"));
+
+        Assert.isTrue(footer.isDisplayed(),"Footer is not visible on the page.");
+
+        int footerY = footer.getLocation().getY();
+        int footerHeight = footer.getSize().getHeight();
+
+        Long totalPageHeight = (Long)((JavascriptExecutor) driver).executeScript("return document.body.scrollHeight");
+
+        Assert.isTrue(footerY + footerHeight  <= totalPageHeight,"Footer is not correctly positioned.");
+    }
+}
