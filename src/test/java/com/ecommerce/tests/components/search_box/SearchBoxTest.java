4ce39a3276ed3e227ddcf00599150060013f83e2
AllaBird <allavorobieva@yahoo.com>
2024-09-24 01:20:01 -0400

    Refactor according meeting

--- src/test/java/com/ecommerce/tests/components/search_box/SearchBoxTest.java
+++ src/test/java/com/ecommerce/tests/components/search_box/SearchBoxTest.java
@@ -1,40 +1,68 @@
 package com.ecommerce.tests.components.search_box;
 
 import com.ecommerce.base.BaseTest;
-import com.ecommerce.data.ProductsData;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.List;
 
 public class SearchBoxTest extends BaseTest {
 
     private static final String ITEM_CATEGORY = "jeans";
 
+    private static int countItemsContainingItemText(List<WebElement> items, String product) {
+        int count = 0;
+        for (WebElement item : items) {
+            String itemText = item.getText().toLowerCase();
+            if (itemText.contains(product.toLowerCase())) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private WebElement waitForElementToBeVisible(By locator) {
+        WebDriverWait wait = new WebDriverWait(driver , Duration.ofSeconds(3));
+
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    private WebElement waitForElementToBeClickable(By locator) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+
+        return wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
     @Test(description = "12-1.1 | TC>Store> Verify Search Returns All Items Across Categories "
             + "# https://app.clickup.com/t/8689vk47d")
     public void testVerifySearchReturnsAllItemsInAllCategories() {
         driver.findElement(By.xpath("//a[@href='/store']")).click();
-        driver.findElement(By.xpath("//input[@type='search']")).sendKeys(ITEM_CATEGORY);
-        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        WebElement searchBox = waitForElementToBeVisible(By.xpath("//input[@type='search']"));
+        searchBox.sendKeys(ITEM_CATEGORY);
+
+        waitForElementToBeClickable(By.xpath("//button[@type='submit']")).click();
 
         List<WebElement> searchResultList = driver.findElements(By.xpath("//ul//h2"));
         Assert.assertFalse(searchResultList.isEmpty(), "Search results are empty.");
-        int countItemBySearch = ProductsData.countItemsContainingItemText(searchResultList, ITEM_CATEGORY);
 
-        driver.findElement(By.xpath("//li[@id='menu-item-1228']//a[text()='Men']")).click();
+        int countItemBySearch = countItemsContainingItemText(searchResultList, ITEM_CATEGORY);
+        waitForElementToBeClickable(By.xpath("//li[@id='menu-item-1228']//a[text()='Men']")).click();
+
         List<WebElement> menItemsList = driver.findElements(By.xpath("//ul//h2"));
-        int countItemInMenResult = ProductsData.countItemsContainingItemText(menItemsList, ITEM_CATEGORY);
+        int countItemInMenResult = countItemsContainingItemText(menItemsList, ITEM_CATEGORY);
 
-        driver.findElement(By.xpath("//li[@id='menu-item-1229']//a[text()='Women']")).click();
+        waitForElementToBeClickable(By.xpath("//li[@id='menu-item-1229']//a[text()='Women']")).click();
         List<WebElement> womenItemsList = driver.findElements(By.xpath("//ul//h2"));
-        int countItemInWomenResult = ProductsData.countItemsContainingItemText(womenItemsList, ITEM_CATEGORY);
+        int countItemInWomenResult = countItemsContainingItemText(womenItemsList, ITEM_CATEGORY);
 
         Assert.assertEquals(countItemBySearch, countItemInMenResult + countItemInWomenResult,
                 "Search box did not find all the items with item name '"
                         + ITEM_CATEGORY + "' or find extra items");
-
     }
 }
+
