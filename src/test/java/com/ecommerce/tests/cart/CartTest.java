ba7e08b3c7c619f94521406c64057f8bd402f115
Anna <am-rabota@mail.ru>
2024-09-30 16:35:06 -0700

    Refactor. Changed waits using WaitUtils methods

--- src/test/java/com/ecommerce/tests/cart/CartTest.java
+++ src/test/java/com/ecommerce/tests/cart/CartTest.java
@@ -1,6 +1,7 @@
 package com.ecommerce.tests.cart;
 
 import com.ecommerce.base.BaseTest;
+import com.ecommerce.utils.WaitUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -38,6 +39,8 @@ public class CartTest extends BaseTest {
             priceStr = priceStr.substring(1); //creating the substring without $
         }
         double price = Double.valueOf(priceStr); //change string price to double
+        double priceForAssert = price * priceIncreeaseToCheck;
+        String priceXPath = String.format("%.2f", priceForAssert);
 
         WebElement quantity = driver.findElement(By.xpath("//input[@class='input-text qty text']"));
         quantity.click();
@@ -47,16 +50,7 @@ public class CartTest extends BaseTest {
         String updatedPriceString = driver.findElement(By.xpath("//td[@class = 'product-subtotal']/span[@class='woocommerce-Price-amount amount']")).getText();
         driver.findElement(By.xpath("//button[@name='update_cart']")).click();
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        String finalUpdatedPriceStr = updatedPriceString;
-        // After the clicking Update price button, I need wait until the price will be updated
-        wait.until(driver -> {
-            String checkPrice = driver.findElement(By.xpath("//td[@class = 'product-subtotal']/span[@class='woocommerce-Price-amount amount']")).getText();
-            if(checkPrice.equals(finalUpdatedPriceStr)) { // checking if price still equal to original price (before update)
-                return false;
-            }
-            return true;  //when price is updated (not equal anymore) returning true
-        });
+        WaitUtils.visibilityOfElementLocated(driver, By.xpath("//td[@class = 'product-subtotal']/span/bdi[contains(text(), '" + priceXPath + "')]"));
 
         //  Getting the updated subtotal price
         String updatedPriceStr = driver.findElement(By.xpath("//td[@class = 'product-subtotal']/span[@class='woocommerce-Price-amount amount']")).getText();
@@ -95,19 +89,7 @@ public class CartTest extends BaseTest {
             product.click();
             counter++;
             final String finalCounter2 = counter.toString();
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-            wait.until(driver -> {
-                try{
-                    WebElement cartAmount = driver.findElement(By.xpath("//span[@class='count']"));
-                    String testValue = cartAmount.getText();
-                    if(testValue.equals(finalCounter2)) {
-                        return true;
-                    }
-                    return false;
-                } catch (Exception e) {
-                    return false;
-                }
-            });
+            WaitUtils.visibilityOfElementLocated(driver, By.xpath("//div[@class='ast-cart-menu-wrap']/span[contains(text(), '" + finalCounter2 +"')]"));
         }
         WebElement viewCart = driver.findElement(By.linkText("View cart"));
         String viewCartText = viewCart.getText();
@@ -120,18 +102,8 @@ public class CartTest extends BaseTest {
             deleteButtons.get(0).click();
             counter--;
             final String finalCounter = counter.toString();
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-            wait.until(driver -> {
-                try {
-                    String testValue = driver.findElement(By.xpath("//span[@class='count']")).getText();
-                    if (testValue.equals(finalCounter)) {
-                        return true;
-                    }
-                    return false;
-                } catch (Exception e) {
-                    return false;
-                }
-            });
+
+            WaitUtils.visibilityOfElementLocated(driver, By.xpath("//div[@class='ast-cart-menu-wrap']/span[contains(text(), '" + finalCounter +"')]"));
             deleteButtons = driver.findElements(By.xpath("//a[@class = 'remove']"));
         }
 
