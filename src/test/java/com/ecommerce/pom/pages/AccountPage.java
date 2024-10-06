9eef620146abe5b630288f5a40ba229e1909d559
ovdro <ovdro.ovdro@gmail.com>
2024-10-06 23:03:18 +0300

    added Account Page

---
+++ src/test/java/com/ecommerce/pom/pages/AccountPage.java
@@ -0,0 +1,39 @@
+package com.ecommerce.pom.pages;
+
+import com.ecommerce.pom.BasePage;
+import com.ecommerce.utils.WaitUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+
+public class AccountPage extends BasePage {
+    By emailField = By.id("//input [ @ id = 'username']");
+    By passwordField = By.id("//input [ @ id = 'password']");
+    By loginButton = By.xpath("//button[@class='woocommerce-button button woocommerce-form-login__submit' and text()='Log in']");
+    By storePageLink = By.id("menu-item-1227");
+    By cartIcon = By.xpath("//span[@class='count']");
+
+    public AccountPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void logIn() throws InterruptedException {
+
+        WaitUtils.visibilityOfElementLocated(getDriver(),emailField).sendKeys("test_test@test.test");
+        WaitUtils.visibilityOfElementLocated(getDriver(),passwordField).sendKeys("12345");
+        WaitUtils.elementToBeClickable(getDriver(),loginButton).click();
+
+    }
+
+    public StorePage navigateToStorePage() {
+        WaitUtils.visibilityOfElementLocated(getDriver(),storePageLink).click();
+
+        return new StorePage(getDriver());
+    }
+    public CartPage navigateToCartPage() {
+        WaitUtils.visibilityOfElementLocated(getDriver(),cartIcon).click();
+
+        return new CartPage(getDriver());
+    }
+
+}
