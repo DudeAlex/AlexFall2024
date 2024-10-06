0edffb1b0d0790b5570b13874b503df7d5fc229b
ovdro <ovdro.ovdro@gmail.com>
2024-10-06 23:07:36 +0300

    added getCartItemsNumber(), removeItemsFromCart()

--- src/test/java/com/ecommerce/pom/pages/CartPage.java
+++ src/test/java/com/ecommerce/pom/pages/CartPage.java
@@ -6,12 +6,27 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class CartPage extends BasePage {
+
     By checkoutButton = By.xpath("//a[@href='https://askomdch.com/checkout/']");
+    By cartIcon = By.xpath("//span[@class='count']");
+    By removeButton = By.xpath("//a[@class='remove']");
+
     public CartPage(WebDriver driver) {
         super(driver);
     }
+
     public CheckoutPage clickCheckoutButton() {
         WaitUtils.visibilityOfElementLocated(getDriver(), checkoutButton).click();
         return new CheckoutPage(getDriver());
+    }
+
+    public String getCartItemsNumber(){
+       return WaitUtils.visibilityOfElementLocated(getDriver(),cartIcon).getText();
+    }
+
+    public void removeItemsFromCart(){
+
+        WaitUtils.elementToBeClickable(getDriver(),removeButton).click();
+
     }
 }
