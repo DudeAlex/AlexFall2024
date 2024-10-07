4c61cd8f296be37464350527e94c94fa40ad062c
ovdro <ovdro.ovdro@gmail.com>
2024-10-07 10:57:00 +0300

    work in progress AddProductWomenPageTest

---
+++ src/test/java/com/ecommerce/pom/pages/WomenPage.java
@@ -0,0 +1,24 @@
+package com.ecommerce.pom.pages;
+
+import com.ecommerce.utils.WaitUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class WomenPage extends SalesPage{
+
+    By addToCartButton  = By.xpath("//div[@class='astra-shop-summary-wrap']//a[text()='Add to cart']");
+    By cartButton = By.linkText("View cart");
+
+    public WomenPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public WomenPage addToCartFromWomenPage() {
+        WaitUtils.visibilityOfElementLocated(getDriver(), addToCartButton).click();
+        return this;
+    }
+    public CartPage clickCartPage() {
+        WaitUtils.visibilityOfElementLocated(getDriver(), cartButton).click();
+        return new CartPage(getDriver());
+    }
+}
