30b350231591d6d81198909d68f643ca6a759d0d
AllaBird <146133797+AllaBird@users.noreply.github.com>
2024-10-08 01:21:47 -0400

    Refactor to POM (#137)

--- src/test/java/com/ecommerce/pom/pages/StorePage.java
+++ src/test/java/com/ecommerce/pom/pages/StorePage.java
@@ -1,11 +1,13 @@
 package com.ecommerce.pom.pages;
 
-import com.ecommerce.pom.BasePage;
 import com.ecommerce.utils.WaitUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class StorePage extends SalesPage {
@@ -15,13 +17,35 @@ public class StorePage extends SalesPage {
 
     By headerTitle = By.xpath("//h1[@class='woocommerce-products-header__title page-title']");
     By loopProducts = By.xpath("//h2[@class='woocommerce-loop-product__title']");
-    By addToCartButton  = By.xpath("//div[@class='astra-shop-summary-wrap']//a[text()='Add to cart']");
+    By addToCartButton = By.xpath("//div[@class='astra-shop-summary-wrap']//a[text()='Add to cart']");
     By cartButton = By.linkText("View cart");
+    By productList = By.xpath("//ul//h2");
+    By nextPageNumber = By.xpath("//a[@class='next page-numbers']");
     By sortBy = By.xpath("//select[@name='orderby']");
     By sortByPrice = By.xpath("//span[@class='byPrice']");
 
     public StorePage(WebDriver driver) {
         super(driver);
+    }
+
+    public List<String> getAllItemsFromAllPages(By locator, WebDriver driver) {
+        List<String> allItemList = new ArrayList<>();
+        boolean hasNextPage = true;
+
+        while (hasNextPage) {
+            List<WebElement> itemList = WaitUtils.visibilityOfAllElementsLocatedBy(driver, locator);
+            for (WebElement item : itemList) {
+                allItemList.add(item.getText());
+            }
+
+            try {
+                WebElement nextPageArrow = driver.findElement(nextPageNumber);
+                nextPageArrow.click();
+            } catch (NoSuchElementException e) {
+                hasNextPage = false;
+            }
+        }
+        return allItemList;
     }
 
     public String getSearchHeaderTitle() {
@@ -37,9 +61,18 @@ public class StorePage extends SalesPage {
         WaitUtils.visibilityOfElementLocated(getDriver(), addToCartButton).click();
         return this;
     }
+
     public CartPage clickCartPage() {
         WaitUtils.visibilityOfElementLocated(getDriver(), cartButton).click();
         return new CartPage(getDriver());
+    }
+
+    public boolean areItemsInAlphabeticalOrder() {
+        List<String> allItemList = getAllItemsFromAllPages(productList, getDriver()); // Получаем все элементы
+        List<String> alphabeticalAllItemList = new ArrayList<>(allItemList); // Создаем копию списка
+        Collections.sort(alphabeticalAllItemList);
+
+        return allItemList.equals(alphabeticalAllItemList);
     }
 
     public StorePage sortByDropDownButton(){
@@ -49,7 +82,7 @@ public class StorePage extends SalesPage {
 
     public StorePage sortByPrice(){
         WaitUtils.visibilityOfElementLocated(getDriver(), sortByPrice).click();
-          return this;
+        return this;
     }
 
     public StorePage chooseAnItemByClickingAddToCart(String item){
@@ -60,4 +93,3 @@ public class StorePage extends SalesPage {
     }
 
 }
-
