afc7804544592f308afaaf318c2bfd852850998f
ovdro <ovdro.ovdro@gmail.com>
2024-10-16 02:54:47 +0300

    refactored Footer Page

--- src/test/java/com/ecommerce/tests/components/footer/FooterForHerTest.java
+++ src/test/java/com/ecommerce/tests/components/footer/FooterForHerTest.java
@@ -2,17 +2,24 @@ package com.ecommerce.tests.components.footer;
 
 import com.ecommerce.base.BaseTest;
 import com.ecommerce.data.FooterForHerData;
+import com.ecommerce.pom.pages.HomePage;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class FooterForHerTest extends BaseTest {
-    @Test(description = "11.1-2.1-5 | TC > Verify Navigation Footer Menu List For Her # https://app.clickup.com/t/8689r60e5",
-            dataProvider = "forHerData", dataProviderClass = FooterForHerData.class)
-    public void testFooterForHerNavigation(String url, String linkXpath){
+    @Test(description = "11.1-2.1-5 | TC > Verify Navigation Footer Menu List For Her # https://app.clickup.com/t/8689r60e5")
+    public void testFooterForHerNavigation(){
 
-        driver.findElement(By.xpath(linkXpath)).click();
+        HomePage homePage = new HomePage(driver);
+        homePage.getFooter().navigateToWomenPageFromFooter();
 
-        Assert.assertEquals(driver.getCurrentUrl(), url);
+        //Assert.assertEquals(driver.getCurrentUrl(), url);
+
+        homePage.getFooter().navigateToWomenJeansFromFooter();
+        homePage.getFooter().navigateToWomenShirtsFromFooter();
+        homePage.getFooter().navigateToWomenShoesFromFooter();
+        homePage.getFooter().navigateToForHerAccessoriesFromFooter();
+
     }
 }
