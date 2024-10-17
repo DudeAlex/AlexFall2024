2598ebfeb6c81a36c5314460c3bb3d99fd4a57c2
GolkaKodi <golkdakoldon17@gmail.com>
2024-10-16 18:36:44 -0700

    add logout

--- src/test/java/com/ecommerce/pom/pages/AccountPage.java
+++ src/test/java/com/ecommerce/pom/pages/AccountPage.java
@@ -5,6 +5,8 @@ import com.ecommerce.pom.Loadable;
 import com.ecommerce.utils.WaitUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Wait;
 
 
 public class AccountPage extends BasePage implements Loadable {
@@ -14,12 +16,12 @@ public class AccountPage extends BasePage implements Loadable {
     By storePageLink = By.id("menu-item-1227");
     By menPageLink = By.id("menu-item-1228");
     By womenPageLink = By.id("menu-item-1229");
-    By cartIcon = By.xpath("//span[@class='count']");   
+    By cartIcon = By.xpath("//span[@class='count']");
     By loginUsername = By.xpath("//input[@id=\"username\"]");
     By loginPassword = By.xpath("//input[@id=\"password\"]");
     By storeButton = By.xpath("//li[@id=\"menu-item-1227\"]/a");
     By homePageLink = By.xpath("//li[@id=\"menu-item-1226\"]/a");
-    By logoutLink = By.xpath("//a[@href = 'https://askomdch.com/account/customer-logout/?_wpnonce=bf33a648b1' and text() = 'Logout']");
+    By logoutLink = By.xpath("//div[@class = 'woocommerce-MyAccount-content']//a[text() = 'Log out']");
 
     By lostPasswordLink = By.xpath("//a[normalize-space()='Lost your password?']");
 
@@ -32,66 +34,63 @@ public class AccountPage extends BasePage implements Loadable {
         getDriver().get("https://askomdch.com/account/");
     }
 
-    public void logIn()
-    {
+    public void logIn() {
         getDriver().findElement(loginUsername).sendKeys("aaaaa@aa.aa");
         getDriver().findElement(loginPassword).sendKeys("11111");
         getDriver().findElement(loginButton).click();
     }
-    public void logIn(String email, String password)
-    {
+
+    public void logIn(String email, String password) {
         getDriver().findElement(loginUsername).sendKeys(email);
         getDriver().findElement(loginPassword).sendKeys(password);
         getDriver().findElement(loginButton).click();
     }
 
     public AccountPage logout() {
-        boolean isLoggedIn = WaitUtils.presenceOfElementLocated(getDriver(), logoutLink).isDisplayed();
-        if (isLoggedIn) {
-            WaitUtils.elementToBeClickable(getDriver(), logoutLink).click();
-        } else {
-            System.out.println("User is not logged in, LogOut link is not present");
+        WebElement logout = WaitUtils.presenceOfElementLocated(getDriver(), logoutLink, 5);
+        if (logout.isDisplayed()) {
+            logout.click();
         }
         return this;
     }
 
     public StorePage navigateToStorePage() {
-        WaitUtils.visibilityOfElementLocated(getDriver(),storePageLink).click();
+        WaitUtils.visibilityOfElementLocated(getDriver(), storePageLink).click();
 
         return new StorePage(getDriver());
     }
 
     public HomePage navigateToHomePage() {
-        WaitUtils.visibilityOfElementLocated(getDriver(),homePageLink).click();
+        WaitUtils.visibilityOfElementLocated(getDriver(), homePageLink).click();
         return new HomePage(getDriver());
     }
 
     public CartPage navigateToCartPage() {
-        WaitUtils.visibilityOfElementLocated(getDriver(),cartIcon).click();
+        WaitUtils.visibilityOfElementLocated(getDriver(), cartIcon).click();
 
         return new CartPage(getDriver());
     }
 
     public MenPage navigateToMenPage() {
-        WaitUtils.visibilityOfElementLocated(getDriver(),menPageLink).click();
+        WaitUtils.visibilityOfElementLocated(getDriver(), menPageLink).click();
 
         return new MenPage(getDriver());
     }
 
     public WomenPage navigateToWomenPage() {
-        WaitUtils.visibilityOfElementLocated(getDriver(),womenPageLink).click();
+        WaitUtils.visibilityOfElementLocated(getDriver(), womenPageLink).click();
 
         return new WomenPage(getDriver());
     }
 
     public AccessoriesPage navigateToAccessoriesPage() {
-        WaitUtils.visibilityOfElementLocated(getDriver(),getHeaderAccessories()).click();
+        WaitUtils.visibilityOfElementLocated(getDriver(), getHeaderAccessories()).click();
         return new AccessoriesPage(getDriver());
     }
 
-    public LostPasswordPage navigateToLostPasswordPage(){
-        WaitUtils.visibilityOfElementLocated(getDriver(),lostPasswordLink).click();
-        return new LostPasswordPage(getDriver()) ;
+    public LostPasswordPage navigateToLostPasswordPage() {
+        WaitUtils.visibilityOfElementLocated(getDriver(), lostPasswordLink).click();
+        return new LostPasswordPage(getDriver());
     }
 
 
