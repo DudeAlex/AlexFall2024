73394d6a97d76e2a1391e4b46959c52d8373655d
Tema S <shvetsartem7@gmail.com>
2024-10-17 22:05:45 -0400

    added the following tests: 3.7-1-1 3.7-1-2 3.7-1-3 3.7-1-4

--- src/test/java/com/ecommerce/pom/pages/MenPage.java
+++ src/test/java/com/ecommerce/pom/pages/MenPage.java
@@ -5,6 +5,7 @@ import com.ecommerce.utils.CollectToListUtils;
 import com.ecommerce.utils.WaitUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
 import java.util.List;
@@ -16,6 +17,10 @@ public class MenPage extends SalesPage{
     By firstProductAddToCartButton = By.xpath("//ul[@class=\"products columns-4\"]//a[2]");
     By cartIcon = By.xpath("//div[@id=\"ast-desktop-header\"]//a[@title=\"View your shopping cart\"]//span");
     By pageHeader = By.tagName("h1");
+    By menQtyInFilter = By.xpath("//select[@id='product_cat']/option[@selected='selected']");
+    By menQtyOnPage = By.cssSelector(".ast-woocommerce-container ul li");
+    By priceFilter = By.cssSelector("#woocommerce_price_filter-3 .price_label");
+    By menItems = By.cssSelector(".ast-woo-product-category");
 
     public MenPage(WebDriver driver) {
         super(driver);
@@ -59,6 +64,31 @@ public class MenPage extends SalesPage{
 
     public String verifyHeaderText(){
         return WaitUtils.visibilityOfElementLocated(getDriver(), pageHeader, 2).getText();
+    }
+
+    public boolean getMenQtyOnPage(){
+        List<WebElement> items = getDriver().findElements(menItems);
+        for( WebElement item : items){
+            if(item.getText().equals("Men")) return true;
+        }
+        return false;
+    }
+
+    public int getMenQtyInFilter(){
+        String filterText = getDriver().findElement(menQtyInFilter).getText();
+        String number = filterText.replaceAll("\\D+", "");
+        return Integer.parseInt(number);
+    }
+
+    public String getPriceRangeText(){
+        String text = getDriver().findElement(priceFilter).getText();
+        return text;
+    }
+
+    public boolean verifyMenItemsCount(int expectedCount) {
+        List<WebElement> items = getDriver().findElements(menItems);
+        int displayedItemCount = items.size();
+        return displayedItemCount == expectedCount;
     }
 }
 
