a972f6d58b1b3258e0bfc933164d0d6d625a18ac
AllaBird <allavorobieva@yahoo.com>
2024-10-18 02:02:51 -0400

    Add uml data for LeftSidebar

--- src/test/java/com/ecommerce/pom/components/LeftSidebar.java
+++ src/test/java/com/ecommerce/pom/components/LeftSidebar.java
@@ -5,18 +5,19 @@ import com.ecommerce.utils.WaitUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class LeftSideMenu extends BaseComponent {
+public class LeftSidebar extends BaseComponent {
 
     By searchInputField = By.xpath("//input[@type='search']");
     By searchButton = By.xpath("//button[@value='Search']");
 
-        public LeftSideMenu(WebDriver driver) {
-            super(driver);
-        }
+    public LeftSidebar(WebDriver driver) {
+        super(driver);
+    }
 
     public StorePage searchProduct(String item) {
         WaitUtils.visibilityOfElementLocated(getDriver(), searchInputField).sendKeys(item);
         WaitUtils.presenceOfElementLocated(getDriver(), searchButton).click();
+
         return new StorePage(getDriver());
     }
 }
