8f7ee7e968891f151532eac0860a25409f72b466
ovdro <ovdro.ovdro@gmail.com>
2024-10-19 22:44:03 +0300

    refactored using EndPoints

--- src/test/java/com/ecommerce/tests/components/footer/FooterQuickLinksTest.java
+++ src/test/java/com/ecommerce/tests/components/footer/FooterQuickLinksTest.java
@@ -5,32 +5,28 @@ import com.ecommerce.pom.pages.HomePage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static com.ecommerce.pom.pages.EndPoints.*;
+
 public class FooterQuickLinksTest extends BaseTest {
     @Test(description = "11.1-1.1-5 | TC > Verify Navigation Footer Menu List Quick Links # https://app.clickup.com/t/8689r5uyy ")
     public void testFooterForHimNavigation(){
 
-        String urlHome = "https://askomdch.com/";
-        String urlAbout = "https://askomdch.com/about/";
-        String urlMyAccount = "https://askomdch.com/account/";
-        String urlCart = "https://askomdch.com/cart/";
-        String urlContactUs = "https://askomdch.com/contact-us/";
-
         HomePage homePage = new HomePage(driver);
 
         homePage.getFooter().navigateToHomepageFromFooter();
-        Assert.assertEquals(driver.getCurrentUrl(), urlHome);
+        Assert.assertEquals(driver.getCurrentUrl(), BASE_URL);
 
         homePage.getFooter().navigateToAboutUsPageFromFooter();
-        Assert.assertEquals(driver.getCurrentUrl(), urlAbout);
+        Assert.assertEquals(driver.getCurrentUrl(), ABOUT_URL);
 
         homePage.getFooter().navigateToMyAccountPageFromFooter();
-        Assert.assertEquals(driver.getCurrentUrl(), urlMyAccount);
+        Assert.assertEquals(driver.getCurrentUrl(), ACCOUNT_URL);
 
         homePage.getFooter().navigateToCartPageFromFooter();
-        Assert.assertEquals(driver.getCurrentUrl(), urlCart);
+        Assert.assertEquals(driver.getCurrentUrl(), CART_URL);
 
         homePage.getFooter().navigateToContactUsPageFromFooter();
-        Assert.assertEquals(driver.getCurrentUrl(), urlContactUs);
+        Assert.assertEquals(driver.getCurrentUrl(), CONTACT_US_URL);
     }
 
 }
