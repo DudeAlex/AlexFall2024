3d7b910807e40f0480602fe1e956817b6b1a4062
ovdro <ovdro.ovdro@gmail.com>
2024-10-19 20:14:13 +0300

    added NumberOfProductIsShownInTheIconTest

---
+++ src/test/java/com/ecommerce/tests/cart/NumberOfProductIsShownInTheIconTest.java
@@ -0,0 +1,34 @@
+package com.ecommerce.tests.cart;
+
+import com.ecommerce.base.BaseTest;
+import com.ecommerce.pojo.User;
+import com.ecommerce.pom.pages.AccountPage;
+import com.ecommerce.pom.pages.CartPage;
+import com.ecommerce.pom.pages.HomePage;
+import com.ecommerce.pom.pages.StorePage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class NumberOfProductIsShownInTheIconTest extends BaseTest {
+
+    @Test(description = "9.1-8-1| TC The number of products in the cart is shown on the cart icon in the header # https://app.clickup.com/t/868abcvg5")
+    public void testNumberOfProductIsShownInTheIconStorePage(){
+        User user = new User("test_test@test.test", "12345");
+        HomePage homePage = new HomePage(driver);
+        AccountPage accountPage = homePage.getHeader().navigateToAccountPage();
+        accountPage.logIn(user.getEmail(), user.getPassword());
+
+        CartPage cartPage = accountPage.getHeader().navigateToCartPage();
+        cartPage.clearTheCart();
+
+        StorePage storePage = cartPage.getHeader().navigateToStorePage();
+        storePage.addToCartFromStorePage();
+        cartPage = storePage.getHeader().navigateToCartPage();
+
+        Assert.assertEquals(cartPage.getCartItemsNumber(), "1");
+
+    }
+
+}
+
