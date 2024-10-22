a9b57ec085b2956718995754f5a8842355ef0ab0
Mike_Bordei <116090785+MythUser@users.noreply.github.com>
2024-10-21 20:02:16 -0700

    Check order history data (#198)

--- src/test/java/com/ecommerce/tests/account/AccountPageOrderHistoryTest.java
+++ src/test/java/com/ecommerce/tests/account/AccountPageOrderHistoryTest.java
@@ -4,20 +4,23 @@ import com.beust.ah.A;
 import com.ecommerce.base.BaseTest;
 import com.ecommerce.pom.pages.AccountOrdersPage;
 import com.ecommerce.pom.pages.AccountPage;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class AccountPageOrderHistoryTest extends BaseTest{
 
         @Test(description = "6.4.1 - 1.1 | TC LogIn with new account to check empty order history ")
-        public void loggedInUserCheckingOrderHistoryOnAccountPage() {
+        public void loggedInUserCheckingEmptyOrderHistoryOnAccountPage() {
 
             String noOrders = "BROWSE PRODUCTS\n" +
                     "No order has been made yet.";
             String browseProducts = "BROWSE PRODUCTS";
 
-            AccountPage accountPage = new AccountPage(driver).load();
-            accountPage.logIn("practice@gmail.com", "Testing1");
+            AccountPage accountPage = new AccountPage(driver);
+            accountPage.load();
+            accountPage.logIn("anyemail@gmail.com", "Testing1");
+
 
             AccountOrdersPage accountOrdersPage = new AccountOrdersPage(driver);
             accountOrdersPage.navigateToAccountOrdersPage();
@@ -29,4 +32,18 @@ public class AccountPageOrderHistoryTest extends BaseTest{
             Assert.assertEquals(noOrdersText, noOrders);
         }
 
+    @Test(description = "6.4.1 - 1.2 | TC LogIn with older account to check order history data")
+    public void loggedInUserCheckingExistingOrderHistoryOnAccountPage() throws InterruptedException {
+
+            AccountPage accountPage = new AccountPage(driver);
+            accountPage.load();
+            accountPage.logIn("practice@gmail.com", "Testing1");
+
+            AccountOrdersPage accountOrdersPage = new AccountOrdersPage(driver);
+            accountOrdersPage.navigateToAccountOrdersPage().ordersTabInfo();
+
+            String viewOrdersButton = accountOrdersPage.navigateToAccountOrdersPage().ordersTabInfo();
+
+            Assert.assertEquals(accountOrdersPage.ordersTabInfo(), viewOrdersButton);
+    }
 }
