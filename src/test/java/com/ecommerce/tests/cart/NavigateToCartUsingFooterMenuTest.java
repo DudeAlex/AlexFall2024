5f85d8e63d99e8d174fd83fbdb9e41b2230eb9ee
ovdro <ovdro.ovdro@gmail.com>
2024-10-23 00:24:22 +0300

    refactor testNavigateToCartUsingFooterMenu

--- src/test/java/com/ecommerce/tests/cart/NavigateToCartUsingFooterMenuTest.java
+++ src/test/java/com/ecommerce/tests/cart/NavigateToCartUsingFooterMenuTest.java
@@ -4,16 +4,19 @@ import com.ecommerce.base.BaseTest;
 import com.ecommerce.pojo.User;
 import com.ecommerce.pom.pages.AccountPage;
 import com.ecommerce.pom.pages.HomePage;
+import com.ecommerce.utils.UserUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
 
 import static com.ecommerce.pom.EndPoints.CART_URL;
 
 public class NavigateToCartUsingFooterMenuTest extends BaseTest {
 
     @Test(description = "9.1-6-1 | TC Customer can view the cart using the quick access link in the footer # https://app.clickup.com/t/868abenum")
-    public void testNavigateToCartUsingFooterMenu() throws InterruptedException {
-        User user = new User("test_test@test.test", "12345");
+    public void testNavigateToCartUsingFooterMenu() throws InterruptedException, IOException {
+        User user = UserUtils.readUserFromJson("user.json");
         HomePage homePage = new HomePage(driver);
         AccountPage accountPage = homePage.getHeader().navigateToAccountPage();
         accountPage.logIn(user.getLogin(), user.getPassword());
