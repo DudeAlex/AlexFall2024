fff46bde7f11c66928d3c4c315413825f7dceaae
ovdro <ovdro.ovdro@gmail.com>
2024-10-26 20:39:05 +0300

    refactor Visible Quick Links pages

--- src/test/java/com/ecommerce/tests/components/footer/FooterVisibleTest.java
+++ src/test/java/com/ecommerce/tests/components/footer/FooterVisibleTest.java
@@ -1,6 +1,7 @@
 package com.ecommerce.tests.components.footer;
 
 import com.ecommerce.base.BaseTest;
+import com.ecommerce.pom.pages.HomePage;
 import dev.failsafe.internal.util.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -11,10 +12,66 @@ import static com.ecommerce.pom.EndPoints.*;
 
 
 public class FooterVisibleTest extends BaseTest {
-    @Test(description = "11.1-6.1 | TC > The footer navigation menu is displayed at the down of every site page # https://app.clickup.com/t/8689wk9pk ")
+    @Test(description = "11.1-1-1 | TC > The footer navigation menu is displayed at the down of every site page # https://app.clickup.com/t/8689wk9pk ")
     public void testFooterVisible() {
 
-        driver.get(BASE_URL);
+        HomePage homePage = new HomePage(driver);
+
+        /**
+         * Quick Links
+         */
+
+        homePage.getFooter().navigateToHomepageFromFooter();
+        org.testng.Assert.assertEquals(driver.getCurrentUrl(), BASE_URL);
+        testIsFooterVisible(BASE_URL);
+
+        homePage.getFooter().navigateToAboutUsPageFromFooter();
+        org.testng.Assert.assertEquals(driver.getCurrentUrl(), ABOUT_URL);
+        testIsFooterVisible(ABOUT_URL);
+
+        homePage.getFooter().navigateToMyAccountPageFromFooter();
+        org.testng.Assert.assertEquals(driver.getCurrentUrl(), ACCOUNT_URL);
+        testIsFooterVisible(ACCOUNT_URL);
+
+        homePage.getFooter().navigateToCartPageFromFooter();
+        org.testng.Assert.assertEquals(driver.getCurrentUrl(), CART_URL);
+        testIsFooterVisible(CART_URL);
+
+        homePage.getFooter().navigateToContactUsPageFromFooter();
+        org.testng.Assert.assertEquals(driver.getCurrentUrl(), CONTACT_US_URL);
+        testIsFooterVisible(CONTACT_US_URL);
+
+        /**
+         * Women's
+         */
+
+        homePage.getFooter().navigateToWomenPageFromFooter();
+        org.testng.Assert.assertEquals(driver.getCurrentUrl(), WOMEN_URL);
+        testIsFooterVisible(WOMEN_URL);
+
+        homePage.getFooter().navigateToWomenJeansFromFooter();
+        org.testng.Assert.assertEquals(driver.getCurrentUrl(), WOMENS_JEANS_URL);
+        testIsFooterVisible(WOMENS_JEANS_URL);
+
+        homePage.getFooter().navigateToWomenShirtsFromFooter();
+        org.testng.Assert.assertEquals(driver.getCurrentUrl(), WOMENS_SHIRTS_URL);
+        testIsFooterVisible(WOMENS_SHIRTS_URL);
+
+
+        homePage.getFooter().navigateToWomenShoesFromFooter();
+        org.testng.Assert.assertEquals(driver.getCurrentUrl(), WOMENS_SHOES_URL);
+        testIsFooterVisible(WOMENS_SHOES_URL);
+
+        homePage.getFooter().navigateToForHerAccessoriesFromFooter();
+        org.testng.Assert.assertEquals(driver.getCurrentUrl(), ACCESSORIES_URL);
+        testIsFooterVisible(ACCESSORIES_URL);
+
+
+    }
+
+    @Test
+    public void testIsFooterVisible (String url){
+        driver.get(url);
 
         WebElement footer = driver.findElement(By.tagName("footer"));
 
