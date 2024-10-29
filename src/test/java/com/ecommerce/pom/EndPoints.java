fce8fb5debf85320dd5834eb468c9ba181018759
ovdro <ovdro.ovdro@gmail.com>
2024-10-29 06:30:14 +0200

    firefox test

--- src/test/java/com/ecommerce/pom/EndPoints.java
+++ src/test/java/com/ecommerce/pom/EndPoints.java
@@ -3,31 +3,31 @@ package com.ecommerce.pom;
 public class EndPoints {
 
     public static final String BASE_URL = "https://askomdch.com/";
-    public static final String STORE_URL = "https://askomdch.com/store/";
-    public static final String MEN_URL = "https://askomdch.com/product-category/men/";
-    public static final String MENS_JEANS_URL = "https://askomdch.com/product-category/mens-jeans/";
-    public static final String MENS_SHIRTS_URL = "https://askomdch.com/product-category/mens-shirts/";
-    public static final String MENS_SHOES_URL = "https://askomdch.com/product-category/mens-shoes/";
-    public static final String WOMEN_URL = "https://askomdch.com/product-category/women/";
-    public static final String WOMENS_JEANS_URL = "https://askomdch.com/product-category/womens-jeans/";
-    public static final String WOMENS_SHIRTS_URL = "https://askomdch.com/product-category/womens-shirts/";
-    public static final String WOMENS_SHOES_URL = "https://askomdch.com/product-category/womens-shoes/";
-    public static final String ACCESSORIES_URL = "https://askomdch.com/product-category/accessories/";
+    public static final String STORE_URL = BASE_URL + "store/";
+    public static final String MEN_URL = BASE_URL + "product-category/men/";
+    public static final String MENS_JEANS_URL = BASE_URL + "product-category/mens-jeans/";
+    public static final String MENS_SHIRTS_URL = BASE_URL + "product-category/mens-shirts/";
+    public static final String MENS_SHOES_URL = BASE_URL + "product-category/mens-shoes/";
+    public static final String WOMEN_URL = BASE_URL + "product-category/women/";
+    public static final String WOMENS_JEANS_URL = BASE_URL + "product-category/womens-jeans/";
+    public static final String WOMENS_SHIRTS_URL = BASE_URL + "product-category/womens-shirts/";
+    public static final String WOMENS_SHOES_URL = BASE_URL + "product-category/womens-shoes/";
+    public static final String ACCESSORIES_URL = BASE_URL + "product-category/accessories/";
 
-    public static final String ACCOUNT_URL = "https://askomdch.com/account/";
-    public static final String ACCOUNT_ORDERS_URL = "https://askomdch.com/account/orders/";
-    public static final String ACCOUNT_DOWNLOADS_URL = "https://askomdch.com/account/downloads/";
-    public static final String ACCOUNT_ADDRESSES_URL = "https://askomdch.com/account/edit-address/";
-    public static final String ACCOUNT_DETAILS_URL = "https://askomdch.com/account/edit-account/";
+    public static final String ACCOUNT_URL = BASE_URL + "account/";
+    public static final String ACCOUNT_ORDERS_URL = BASE_URL + "orders/";
+    public static final String ACCOUNT_DOWNLOADS_URL = BASE_URL + "downloads/";
+    public static final String ACCOUNT_ADDRESSES_URL = BASE_URL + "edit-address/";
+    public static final String ACCOUNT_DETAILS_URL = BASE_URL + "edit-account/";
 
-    public static final String ABOUT_URL = "https://askomdch.com/about/";
-    public static final String CONTACT_US_URL = "https://askomdch.com/contact-us/";
-    public static final String CART_URL = "https://askomdch.com/cart/";
+    public static final String ABOUT_URL = BASE_URL + "about/";
+    public static final String CONTACT_US_URL = BASE_URL + "contact-us/";
+    public static final String CART_URL = BASE_URL + "cart/";
 
-    public static final String CHECKOUT_URL = "https://askomdch.com/checkout/";
-    public static final String LOST_PASSWORD_URL = "https://askomdch.com/account/lost-password/";
+    public static final String CHECKOUT_URL = BASE_URL + "checkout/";
+    public static final String LOST_PASSWORD_URL = BASE_URL + "account/lost-password/";
 
-    public static final String EDIT_ACCOUNT_URL = "https://askomdch.com/account/edit-account/";
+    public static final String EDIT_ACCOUNT_URL = BASE_URL + "account/edit-account/";
 
 
 
