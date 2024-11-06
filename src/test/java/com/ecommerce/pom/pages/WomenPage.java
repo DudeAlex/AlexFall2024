ae25907f6c2922e218bc783b3413fb74f1569942
GolkaKodi <golkdakoldon17@gmail.com>
2024-11-05 17:21:19 -0800

    add testProductImage()

--- src/test/java/com/ecommerce/pom/pages/WomenPage.java
+++ src/test/java/com/ecommerce/pom/pages/WomenPage.java
@@ -1,15 +1,18 @@
 package com.ecommerce.pom.pages;
 
 import com.ecommerce.pom.Loadable;
+import com.ecommerce.utils.JSExecutorUtils;
 import com.ecommerce.utils.WaitUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import static com.ecommerce.pom.EndPoints.WOMEN_URL;
 
 public class WomenPage extends SalesPage<WomenPage> {
 
     By itemsResultNumber = By.cssSelector(".woocommerce-result-count");
+    By denimBlueJeansImage = By.xpath("//a[contains(@href, 'blue-jeans/') and contains(@class, 'LoopProduct-link')]/img");
 
         public WomenPage(WebDriver driver) {
         super(driver);
@@ -26,5 +29,14 @@ public class WomenPage extends SalesPage<WomenPage> {
         String result = WaitUtils.presenceOfElementLocated(getDriver(), itemsResultNumber).getText();
         result = result.replaceAll("[A-Za-z\\s]", "");
         return Integer.parseInt(result);
+    }
+    public String getImageAttribute(String attribute) {
+        WebElement image = WaitUtils.visibilityOfElementLocated(getDriver(), denimBlueJeansImage);
+        String attributeOfImage = image.getAttribute(attribute);
+        return attributeOfImage;
+    }
+
+    public void scrollToOneThirdOfWomenPage() {
+        JSExecutorUtils.scrollToOneThirdOfPage(getDriver());
     }
 }
