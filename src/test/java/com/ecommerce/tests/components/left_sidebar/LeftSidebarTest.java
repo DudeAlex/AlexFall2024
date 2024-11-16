2efa69fc94c5369892d1ad56ac9079a3d81b1334
AllaBird <allavorobieva@yahoo.com>
2024-11-16 17:39:35 -0500

    Refactor

--- src/test/java/com/ecommerce/tests/components/left_sidebar/LeftSidebarTest.java
+++ src/test/java/com/ecommerce/tests/components/left_sidebar/LeftSidebarTest.java
@@ -17,7 +17,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import java.util.ArrayList;
